74acdc553ac7a4d693fbfcce412dff0eec4da43b
Jumpman <41526355+JumpmanSr@users.noreply.github.com>
2018-07-21 18:54:57 -0400

    Add files via upload

---
+++ BailOut-master/BailOutMode/BailOutModePlugin.cs
@@ -0,0 +1,53 @@
+﻿using IllusionPlugin;
+using System;
+using System.Reflection;
+using BailOutMode.Patches;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BailOutModePlugin : IPlugin
+{
+    public string Name => "BailOut Mode BS(0.11.1 compatible)";
+    public string Version => "0.0.3.420";
+
+    public static bool BailedOut = false;
+
+    static BailOutModePlugin()
+    {
+
+    }
+
+    public void OnApplicationStart()
+    {
+        Console.WriteLine("[Bailout] Starting Bailout");
+        GameEnergyPatches.PatchMethods();
+        MenuMasterViewControllerPatches.PatchMethods();// disabled 4 now because fuck u beatsaber // jk i fixed it, but I didn't rename shit sorry :(
+        Console.WriteLine("[Bailout] Assembly Patched");
+    }
+
+    public void OnApplicationQuit()
+    {
+
+    }
+
+    public void OnLevelWasLoaded(int level)
+    {
+
+    }
+
+    public void OnLevelWasInitialized(int level)
+    {
+
+    }
+
+    public void OnUpdate()
+    {
+
+    }
+
+    public void OnFixedUpdate()
+    {
+
+    }
+
+}
---
+++ BailOut-master/BailOutMode/Patches/GameEnergyPatches.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reflection;
+using Ryder.Lightweight;
+
+class GameEnergyPatches
+{
+    private static Redirection addEnergyRedirection;
+
+    public static void PatchMethods()
+    {
+        MethodInfo addEnergyInfo =
+            typeof(GameEnergyCounter).GetMethod("AddEnergy", BindingFlags.Public | BindingFlags.Instance);
+        MethodInfo addEnergyPatch =
+            typeof(GameEnergyPatches).GetMethod(nameof(AddEnergyPatch), BindingFlags.NonPublic | BindingFlags.Static);
+        addEnergyRedirection = new Redirection(addEnergyInfo, addEnergyPatch, true);
+    }
+
+    private static void AddEnergyPatch(object self, float value)
+    {
+        GameEnergyCounter instance = (GameEnergyCounter) self;
+        if (!(instance.energy + value <= 1E-05f) || BailOutModePlugin.BailedOut)
+        {
+            addEnergyRedirection.InvokeOriginal(self, value);
+            return;
+        }
+
+        Console.WriteLine("[Bailout] Lethal energy reached, bailing out!");
+        BailOutModePlugin.BailedOut = true;
+
+        try
+        {
+            typeof(GameEnergyCounter).GetField("_cannotFail", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(instance, true);
+
+            var sceneSetup = UnityEngine.Object.FindObjectOfType<MainGameSceneSetup>();
+            MainGameSceneSetupData setupData = typeof(MainGameSceneSetup).GetField("_mainGameSceneSetupData", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(sceneSetup) as MainGameSceneSetupData;
+            setupData.gameplayOptions.noEnergy = true;
+
+            UnityEngine.Object.FindObjectOfType<GameEnergyUIPanel>().EnableEnergyPanel(false);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        value = 0;
+        addEnergyRedirection.InvokeOriginal(self, value);
+    }
+    //Shoutout to foolish dave for all of this.
+}
